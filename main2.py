db767c18370f7a5cd6f58c14ec9716fb455e11b8
xxxvik-xakerxxx <wolfis_rain@protonmail.com>
2019-04-09 13:16:16 +0300

    new adaptive desing, small code line

--- main2.py
+++ main2.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# TODO: отфиксить это говно. Почти отфиксил, накидал еще пару говнострок.
+# TODO: Хоть и говнокод но работает, пока работает)
 # TODO: перевести его в WEB стезю. Отказатся от django перейти на Flask
 # (да я мазахист)
 import sys
-from PyQt5.QtWidgets import QComboBox, QLineEdit, QApplication, QWidget
+from PyQt5.QtWidgets import QComboBox, QLineEdit, QApplication, QWidget, QMainWindow, QFormLayout
 from PyQt5 import QtCore
-from res.ui2 import Ui_Dialog
+from res.ui2t import Ui_MainWindow
 from res.base import Ui_Dialog2
 import sqlite3
+
 
 
 class BaseWind(QWidget):
@@ -18,13 +19,13 @@ class BaseWind(QWidget):
         self.ui2.setupUi(self)
 
 
-class MyApp(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
-        self.ui = Ui_Dialog()
+class MyApp(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyApp, self).__init__(parent=parent)
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.pushButton.clicked.connect(self.test)
+        self.ui.pushButton.clicked.connect(self.calc)
         self.ui.pushButton_3.clicked.connect(self.show_basewind)
         self.ui.pushButton_4.clicked.connect(app.quit)
         self.ui.pushButton_5.clicked.connect(self.add_pol)
@@ -34,10 +35,9 @@ class MyApp(QWidget):
         self.dialog1.show()
 
     def add_pol(self):
-        i = int(self.ui.lineEdit.text())
+        self.komp_item = int(self.ui.lineEdit.text())
         v_pos = 40
         namber_com = 2
-        # namber_lin = 2
         component_list = [
             "None",
             "Пшеница",
@@ -56,47 +56,49 @@ class MyApp(QWidget):
             "Премикс8184",
             "Премикс8201"
         ]
-        print(component_list)   # проверка что получилось
-
+        self.all_combox = []
+        self.all_lineEd = []
+        i = self.komp_item
         while (i > 0):
 
             """Создание combo_Box по количеству указанному в lineEdit"""
-            print('Вход в цикл...Проверка')
-            combo_Box = 'self.comboBox_' + str(namber_com)
-            line_Edit = 'self.LineEdit_' + str(namber_com)
-            print(combo_Box)  # проверка имен combo_Box
-            print(line_Edit)  # проверка имен line_Edit
-            combo_Box = QComboBox(self)
-            combo_Box.setGeometry(QtCore.QRect(0, int(v_pos), 81, 22))
-            combo_Box.setObjectName('comboBox_'+str(namber_com))
-            com = [combo_Box.objectName()]
 
-            com_b = tuple(str(com))  # вычисляем порядковый номер комбобокса
-            # print(com_b)
-            com1 = com_b[11]
-            print(com1)
-            # com_b = {com1: line_edit.text() for i in range(i)}
-            # print(com_b)
-            combo_Box.addItems(component_list)
-            combo_Box.show()
-            line_Edit = QLineEdit(self)
-            line_Edit.setGeometry(QtCore.QRect(90, int(v_pos), 76, 22))
-            line_Edit.show()
-            print('Выход с цикла')
+            combo_Box1 = 'comboBox_' + str(namber_com)
+            line_Edit1 = 'LineEdit_' + str(namber_com)
+
+
+            component_tab_FormLayout = i + 1 #нужен для сдвига компонентов на макете
+
+            self.combo_Box = QComboBox(self.ui.verticalFrame_3)
+            self.combo_Box.setObjectName('comboBox_'+str(namber_com))
+
+            self.combo_Box.addItems(component_list)
+            self.ui.formLayout.setWidget(component_tab_FormLayout, QFormLayout.LabelRole, self.combo_Box)
+            self.combo_Box.show()
+            self.line_Edit = QLineEdit(self.ui.verticalFrame_3)
+            self.ui.formLayout.setWidget(component_tab_FormLayout, QFormLayout.FieldRole, self.line_Edit)
+            self.line_Edit.show()
+            self.all_combox.append(self.combo_Box)
+            self.all_lineEd.append(self.line_Edit)
+
+
             v_pos += 30
             namber_com += 1
-            # namber_lin += 1
             i -= 1
 
-    def test(self):
-        namber = int(self.ui.lineEdit.text()) + 1
-        while namber > 1:
-            print("След цикл")
-            combox = "self.comboBox_" + str(add_pol.namber_com)
-            namber -= 1
-            print(combox)
-            index = combox.currentText()
-            print(index)
+
+    #def test(self):
+    #    print(self.all_lineEd)
+    #    print(self.all_combox)
+    #    namber = int(self.ui.lineEdit.text()) - 1
+    #    while namber >= 0:
+    #        print("След цикл")
+    #        combox = self.all_combox[namber]
+    #        lineEd = self.all_lineEd[namber]
+    #        namber -= 1
+    #        index = combox.currentText()
+    #        nam = lineEd.text()
+    #        print(index, nam)
 
     def calc(self):
 
@@ -120,23 +122,47 @@ class MyApp(QWidget):
 
     def sum_components(self):
         _translate = QtCore.QCoreApplication.translate
-        phen = float(self.ui.lineEdit.text())
-        yach = float(self.ui.lineEdit_2.text())
-        kuk = float(self.ui.lineEdit_3.text())
-        m_k = float(self.ui.lineEdit_4.text())
-        fish = float(self.ui.lineEdit_5.text())
-        prem_8184 = float(self.ui.lineEdit_6.text())
-        gmih = float(self.ui.lineEdit_7.text())
-        shr = float(self.ui.lineEdit_8.text())
-        mk2 = float(self.ui.lineEdit_9.text())
-        yam = float(self.ui.lineEdit_10.text())
-        trm = float(self.ui.lineEdit_11.text())
-        msrs = float(self.ui.lineEdit_12.text())
-        r_k = float(self.ui.lineEdit_13.text())
-        prem_8203 = float(self.ui.lineEdit_14.text())
-        prem_8201 = float(self.ui.lineEdit_15.text())
-        end = (phen + yach + kuk + m_k + fish + prem_8184 + gmih
-               + shr + mk2 + yam + trm + msrs + r_k + prem_8203 + prem_8201)
+
+        self.komponents = [   ["None", 0],
+            ["Пшеница", 0],
+            ["Ячмень", 0],
+            ["Кукуруза", 0],
+            ["Рыбная", 0],
+            ["Мясо/кос^", 0],
+            ["Мясо/кос", 0],
+            ["Яичная мука", 0],
+            ["Жмых п-сол.", 0],
+            ["Шрот соевый", 0],
+            ["Масло раст.", 0],
+            ["Мука трав.", 0],
+            ["Ракушка", 0],
+            ["Премикс8203", 0],
+            ["Премикс8184", 0],
+            ["Премикс8201", 0]]
+
+        namber = int(self.komp_item) - 1
+        #TODO: колхоз надо бы переделать
+        while namber >= 0:
+            combox = self.all_combox[namber]
+            lineEd = self.all_lineEd[namber]
+            index = combox.currentText()
+            nam = float(lineEd.text())
+            i = (len(self.komponents)) - 1
+
+            while i >= 0:
+                if index == self.komponents[i][0]:
+                    self.komponents[i][1] = nam
+                i -= 1
+            namber -= 1
+
+        end = 0
+        komponent_buf = []
+        i = (len(self.komponents)) - 1
+        while i >= 0:
+            if self.komponents[i][1] > 0:
+                end = end + self.komponents[i][1]
+                komponent_buf.append(self.komponents[i])
+            i -= 1
 
         if end == 100:
             self.ui.label_12.setStyleSheet("color: rgb(0, 255, 60);")
@@ -146,78 +172,41 @@ class MyApp(QWidget):
             self.ui.label_12.setStyleSheet("color: rgb(255, 0, 0);")
 
             self.ui.label_12.setText(_translate("Dialog", str(end)))
-        self.open_bd(phen, yach, kuk, m_k, fish, prem_8184,
-                     gmih, shr, mk2, yam, trm, msrs, r_k, prem_8203, prem_8201)
+        self.open_bd(komponent_buf)
 
-    def calculate_equation(self, row, defaults):
-        val_phen = row[0][0]
-        val_yach = row[1][0]
-        val_kuk = row[2][0]
-        val_fish = row[3][0]
-        val_mk2 = row[4][0]
-        val_m_k = row[5][0]
-        val_yam = row[6][0]
-        val_gmih = row[7][0]
-        val_shr = row[8][0]
-        val_msrs = row[9][0]
-        val_trm = row[10][0]
-        val_r_k = row[11][0]
-        val_prem_8203 = row[12][0]
-        val_prem_8184 = row[13][0]
-        val_prem_8201 = row[14][0]
+    def calculate_equation(self, row, komponent_buf):
 
-        (phen,
-         yach,
-         kuk,
-         fish,
-         mk2,
-         m_k,
-         yam,
-         gmih,
-         shr,
-         msrs,
-         trm,
-         r_k,
-         prem_8203,
-         prem_8184,
-         prem_8201) = defaults
+        row_buf = [
+            ["Пшеница", 0],
+            ["Ячмень",  1],
+            ["Кукуруза", 2],
+            ["Рыбная", 3],
+            ["Мясо/кос^", 4],
+            ["Мясо/кос",  5],
+            ["Яичная мука", 6],
+            ["Жмых п-сол.", 7],
+            ["Шрот соевый", 8],
+            ["Масло раст.", 9],
+            ["Мука трав.", 10],
+            ["Ракушка", 11],
+            ["Премикс8203", 12],
+            ["Премикс8184", 14],
+            ["Премикс8201", 15]
+        ]
 
-        return ((val_phen * phen)
-                + (val_yach * yach)
-                + (val_kuk * kuk)
-                + (val_m_k * m_k)
-                + (val_fish * fish)
-                + (val_prem_8184 * prem_8184)
-                + (val_prem_8203 * prem_8203)
-                + (val_prem_8201 * prem_8201)
-                + (val_gmih * gmih)
-                + (val_shr * shr)
-                + (val_mk2 * mk2)
-                + (val_yam * yam)
-                + (val_trm * trm)
-                + (val_msrs * msrs)
-                + (val_r_k * r_k)) / 100
+        x =  (len(row_buf)) - 1
+        pitat_wesch = 0
+        while x >= 0:
+            j =  (len(komponent_buf)) - 1
+            while j >= 0:
+                if row_buf[x][0] == komponent_buf[j][0]:
+                    pitat_wesch += (float(row[int(row_buf[x][1])][0]) * float(komponent_buf[j][1]) )
+                j -= 1
+            x -= 1
 
-    def open_bd(self, phen, yach, kuk, m_k, fish, prem_8184,
-                gmih, shr, mk2, yam, trm, msrs, r_k, prem_8203, prem_8201):
+        return (pitat_wesch) / 100
 
-        defaults = (
-            phen,
-            yach,
-            kuk,
-            fish,
-            mk2,
-            m_k,
-            yam,
-            gmih,
-            shr,
-            msrs,
-            trm,
-            r_k,
-            prem_8203,
-            prem_8184,
-            prem_8201
-        )
+    def open_bd(self, komponent_buf):
 
         conn = sqlite3.connect('./data/data.db')
         c = conn.cursor()
@@ -250,14 +239,14 @@ class MyApp(QWidget):
         c.close()
         conn.close()
 
-        kdg = self.calculate_equation(row_kdg, defaults)
-        sp = self.calculate_equation(row_sp, defaults)
-        sk = self.calculate_equation(row_sk, defaults)
-        cal = self.calculate_equation(row_cal, defaults)
-        fos = self.calculate_equation(row_fos, defaults)
-        na = self.calculate_equation(row_na, defaults)
-        liz = self.calculate_equation(row_liz, defaults)
-        met = self.calculate_equation(row_met, defaults)
+        kdg = self.calculate_equation(row_kdg, komponent_buf)
+        sp = self.calculate_equation(row_sp, komponent_buf)
+        sk = self.calculate_equation(row_sk, komponent_buf)
+        cal = self.calculate_equation(row_cal, komponent_buf)
+        fos = self.calculate_equation(row_fos, komponent_buf)
+        na = self.calculate_equation(row_na, komponent_buf)
+        liz = self.calculate_equation(row_liz, komponent_buf)
+        met = self.calculate_equation(row_met, komponent_buf)
 
         self.calc_end(kdg, sp, sk, cal, fos, na, liz, met)
 
@@ -323,28 +312,28 @@ class MyApp(QWidget):
         ptic_liz = row_liz[n][0]
         ptic_met = row_met[n][0]
 
-        self.ui.label_26.setText(_translate("Dialog", str(ptic_kdg)))
-        self.ui.label_27.setText(_translate("Dialog", str(ptic_sp)))
-        self.ui.label_28.setText(_translate("Dialog", str(ptic_sk)))
-        self.ui.label_29.setText(_translate("Dialog", str(ptic_cal)))
-        self.ui.label_30.setText(_translate("Dialog", str(ptic_fos)))
-        self.ui.label_31.setText(_translate("Dialog", str(ptic_na)))
-        self.ui.label_49.setText(_translate("Dialog", str(ptic_liz)))
-        self.ui.label_50.setText(_translate("Dialog", str(ptic_met)))
+        self.ui.label_26.setText(_translate("Dialog", str(float("{0:.3f}".format(ptic_kdg)))))
+        self.ui.label_27.setText(_translate("Dialog", str(float("{0:.3f}".format(ptic_sp)))))
+        self.ui.label_28.setText(_translate("Dialog", str(float("{0:.3f}".format(ptic_sk)))))
+        self.ui.label_29.setText(_translate("Dialog", str(float("{0:.3f}".format(ptic_cal)))))
+        self.ui.label_30.setText(_translate("Dialog", str(float("{0:.3f}".format(ptic_fos)))))
+        self.ui.label_31.setText(_translate("Dialog", str(float("{0:.3f}".format(ptic_na)))))
+        self.ui.label_49.setText(_translate("Dialog", str(float("{0:.3f}".format(ptic_liz)))))
+        self.ui.label_50.setText(_translate("Dialog", str(float("{0:.3f}".format(ptic_met)))))
 
         self.sum_components()
 
     def calc_end(self, kdg, sp, sk, cal, fos, na, liz, met):
         _translate = QtCore.QCoreApplication.translate
 
-        self.ui.label_20.setText(_translate("Dialog", str(kdg)))
-        self.ui.label_21.setText(_translate("Dialog", str(sp)))
-        self.ui.label_22.setText(_translate("Dialog", str(sk)))
-        self.ui.label_23.setText(_translate("Dialog", str(cal)))
-        self.ui.label_24.setText(_translate("Dialog", str(fos)))
-        self.ui.label_25.setText(_translate("Dialog", str(na)))
-        self.ui.label_47.setText(_translate("Dialog", str(liz)))
-        self.ui.label_48.setText(_translate("Dialog", str(met)))
+        self.ui.label_20.setText(_translate("Dialog", str(float("{0:.3f}".format(kdg)))))
+        self.ui.label_21.setText(_translate("Dialog", str(float("{0:.3f}".format(sp)))))
+        self.ui.label_22.setText(_translate("Dialog", str(float("{0:.3f}".format(sk)))))
+        self.ui.label_23.setText(_translate("Dialog", str(float("{0:.3f}".format(cal)))))
+        self.ui.label_24.setText(_translate("Dialog", str(float("{0:.3f}".format(fos)))))
+        self.ui.label_25.setText(_translate("Dialog", str(float("{0:.3f}".format(na)))))
+        self.ui.label_47.setText(_translate("Dialog", str(float("{0:.3f}".format(liz)))))
+        self.ui.label_48.setText(_translate("Dialog", str(float("{0:.3f}".format(met)))))
 
         norm_kdg = float(self.ui.label_26.text())
         norm_sp = float(self.ui.label_27.text())
@@ -358,66 +347,66 @@ class MyApp(QWidget):
         otl_kdg = norm_kdg - kdg
         if kdg == norm_kdg:
             self.ui.label_32.setStyleSheet("color: rgb(0, 255, 60);")
-            self.ui.label_32.setText(_translate("Dialog", str(otl_kdg)))
+            self.ui.label_32.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_kdg)))))
         else:
             self.ui.label_32.setStyleSheet("color: rgb(255, 0, 0);")
-            self.ui.label_32.setText(_translate("Dialog", str(otl_kdg)))
+            self.ui.label_32.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_kdg)))))
 
         otl_sp = norm_sp - sp
         if sp == norm_sp:
             self.ui.label_33.setStyleSheet("color: rgb(0, 255, 60);")
-            self.ui.label_33.setText(_translate("Dialog", str(otl_sp)))
+            self.ui.label_33.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_sp)))))
         else:
             self.ui.label_33.setStyleSheet("color: rgb(255, 0, 0);")
-            self.ui.label_33.setText(_translate("Dialog", str(otl_sp)))
+            self.ui.label_33.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_sp)))))
 
         otl_sk = norm_sk - sk
         if sk == norm_sk:
             self.ui.label_34.setStyleSheet("color: rgb(0, 255, 60);")
-            self.ui.label_34.setText(_translate("Dialog", str(otl_sk)))
+            self.ui.label_34.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_sk)))))
         else:
             self.ui.label_34.setStyleSheet("color: rgb(255, 0, 0);")
-            self.ui.label_34.setText(_translate("Dialog", str(otl_sk)))
+            self.ui.label_34.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_sk)))))
 
         otl_cal = norm_cal - cal
         if cal == norm_cal:
             self.ui.label_35.setStyleSheet("color: rgb(0, 255, 60);")
-            self.ui.label_35.setText(_translate("Dialog", str(otl_cal)))
+            self.ui.label_35.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_cal)))))
         else:
             self.ui.label_35.setStyleSheet("color: rgb(255, 0, 0);")
-            self.ui.label_35.setText(_translate("Dialog", str(otl_cal)))
+            self.ui.label_35.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_cal)))))
 
         otl_fos = norm_fos - fos
         if fos == norm_fos:
             self.ui.label_36.setStyleSheet("color: rgb(0, 255, 60);")
-            self.ui.label_36.setText(_translate("Dialog", str(otl_fos)))
+            self.ui.label_36.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_fos)))))
         else:
             self.ui.label_36.setStyleSheet("color: rgb(255, 0, 0);")
-            self.ui.label_36.setText(_translate("Dialog", str(otl_fos)))
+            self.ui.label_36.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_fos)))))
 
         otl_na = norm_na - na
         if na == norm_na:
             self.ui.label_37.setStyleSheet("color: rgb(0, 255, 60);")
-            self.ui.label_37.setText(_translate("Dialog", str(otl_na)))
+            self.ui.label_37.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_na)))))
         else:
             self.ui.label_37.setStyleSheet("color: rgb(255, 0, 0);")
-            self.ui.label_37.setText(_translate("Dialog", str(otl_na)))
+            self.ui.label_37.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_na)))))
 
         otl_liz = norm_liz - liz
         if liz == norm_liz:
             self.ui.label_51.setStyleSheet("color: rgb(0, 255, 60);")
-            self.ui.label_51.setText(_translate("Dialog", str(otl_liz)))
+            self.ui.label_51.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_liz)))))
         else:
             self.ui.label_51.setStyleSheet("color: rgb(255, 0, 0);")
-            self.ui.label_51.setText(_translate("Dialog", str(otl_liz)))
+            self.ui.label_51.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_liz)))))
 
         otl_met = norm_met - met
         if met == norm_met:
             self.ui.label_52.setStyleSheet("color: rgb(0, 255, 60);")
-            self.ui.label_52.setText(_translate("Dialog", str(otl_met)))
+            self.ui.label_52.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_met)))))
         else:
             self.ui.label_52.setStyleSheet("color: rgb(255, 0, 0);")
-            self.ui.label_52.setText(_translate("Dialog", str(otl_met)))
+            self.ui.label_52.setText(_translate("Dialog", str(float("{0:.3f}".format(otl_met)))))
 
 
 if __name__ == "__main__":
